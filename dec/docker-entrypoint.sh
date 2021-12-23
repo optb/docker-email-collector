80de5967e042b11d3d9d2649fdcbbf2e029dbb86
Ilja <ilja@kukisch.de>
2021-12-23 17:14:28 +0100

    added defaults to fetchmailrc; small changes in setup;

--- dec/docker-entrypoint.sh
+++ dec/docker-entrypoint.sh
@@ -3,28 +3,28 @@ set -e
 
 if [ "$1" = 'start' ]; then
   
+  echo "Starting docker-email-collector..."
   chmod 0555 /dec
 
+  echo "- setup files and permissions"
   chmod 0400 /etc/fetchmail
   chmod u+x /dec/setup.sh
   /dec/setup.sh
     
+  echo "- compile global sieve scripts"
   sievec /etc/dovecot/sieve/
   sievec /etc/dovecot/sieve/before.d/
   sievec /etc/dovecot/sieve/after.d/
 
-  mkdir -p -- /srv/mail
-  chown vmail:vmail /srv/mail
-  chmod 0770 /srv/mail
-  mkdir -p -- /var/lib/rspamd
-  chown _rspamd:_rspamd /var/lib/rspamd
-  chmod 0770 /var/lib/rspamd
 
   if [[ "${FETCHMAIL_ENABLED}" = "1" ]]; then
+    echo "- start fetchmail"
     /dec/fetchmail/start.sh
   fi
 
+  echo "- start rspamd"
   /etc/init.d/rspamd start
+  echo "- start dovecot"
   /usr/sbin/dovecot -F
 fi
 
--- dec/setup.sh
+++ dec/setup.sh
@@ -12,13 +12,14 @@ cat /etc/fetchmail/fetchmailrc > /etc/fetchmailrc
 
 for cfile in /etc/fetchmail/jobs/*
 do
-  cat "$cfile" >> /etc/fetchmailrc
   echo -e "\n\n" >> /etc/fetchmailrc
+  cat "$cfile" >> /etc/fetchmailrc
 done
 
 chown fetchmail /etc/fetchmailrc
 chmod 0400 /etc/fetchmailrc
 
+# setup log files
 mkdir -p -- /var/log/fetchmail
 touch /var/log/fetchmail/fetchmail.log
 chown -R fetchmail:vmail /var/log/fetchmail
@@ -29,6 +30,14 @@ touch /var/log/dovecot/dovecot.log
 chown -R vmail:vmail /var/log/dovecot
 chmod -R 0770 /var/log/dovecot
 
-# Setup rspamd
+# setup rspamd
 chmod 0555 /dec/rspamd/learn-ham.sh
 chmod 0555 /dec/rspamd/learn-spam.sh
+
+# setup data folders for mail and spamd
+mkdir -p -- /srv/mail
+chown vmail:vmail /srv/mail
+chmod 0770 /srv/mail
+mkdir -p -- /var/lib/rspamd
+chown _rspamd:_rspamd /var/lib/rspamd
+chmod 0770 /var/lib/rspamd
