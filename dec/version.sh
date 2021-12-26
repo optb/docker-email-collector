3e5a73d3c7e926db8d795de2d476079911943d15
Ilja <ilja@kukisch.de>
2021-12-26 01:20:14 +0100

    improved version.sh

--- dec/version.sh
+++ dec/version.sh
@@ -1,4 +1,12 @@
-echo "Dovecot version: "
+#! /bin/sh
+
+echo "Dovecot:"
 dovecot --version
+echo "---"
+
+echo "Rspamd:"
 rspamd --version
+echo "---"
+
+echo "Fetchmail:"
 fetchmail --version 1|grep release
